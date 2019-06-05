69f3e8c70f97ddf5af10cb428a2525ea9e367a7f
Pedro Verly <pedrofverly@hotmail.com>
2019-06-04 22:20:14 -0300

    Update KeySchedule.ino
    Alteração keyschedule

--- KeySchedule.ino
+++ KeySchedule.ino
@@ -1,68 +1,77 @@
-byte subchaves[4][40];
-//byte vettemp_key[4][4];
-int _Wi = 4;
-int _ColunaRcon = 0;
-byte SubBytesKey(byte c[]);
-
-static const byte Rcon[4][10]  = {{0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36},
-                                  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-                                  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-                                  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-                                  };
-
-void KeySchedule(byte chave[4][4]){
-  int linha=4, coluna, i;
+void KeySchedule(byte chave[][4], int round){  
+  byte Rcon[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
   
-  
-//subchaves recebe a chave original
-if(_Wi == 4)
-  {
-     for(linha=0; linha<4 ; linha++){
-    for(coluna=0;coluna<4; coluna++){
-      subchaves[linha][coluna] = chave[linha][coluna];     
-    }    
-  }
-  }
-  
-  //RotWord
-  byte rotword[4][1] = { {subchaves[1][_Wi-1]}, {subchaves[2][_Wi-1]}, {subchaves[3][_Wi-1]}, {subchaves[0][_Wi-1]} };
-  //Passando da matriz rotword para o vetor chave 
-  byte chave2[4] = {rotword[0][0],rotword[1][0],rotword[2][0],rotword[3][0]};
-  
-  //SubByte na Coluna WI
-  SubBytesKey(chave2);
+  byte sub(byte valor);
 
-  //XOR
-  for(linha=0; linha<4 ; linha++){      
-      chave2[linha] = subchaves[linha][_Wi-4] ^ chave2[linha] ^ Rcon[linha][_ColunaRcon];
-  }
-  
-  //incrementando Rcon
-  _ColunaRcon++;
+  byte copia[4];
+  byte matrizcopia[4][4];
 
-  //subchaves recebe Wi
-   for(linha=0; linha<4 ; linha++){      
-      subchaves[linha][_Wi] = chave2[linha];
-  }
-
-  //incrementando wi
-  _Wi++;
-
-  // XOR Wi-4 ^ Wi-1
-   i = 0;
-   while(i<3){
-     for(linha=0; linha<4 ; linha++){      
-      subchaves[linha][_Wi] = subchaves[linha][_Wi-4] ^ subchaves[linha][_Wi-1];     
-  }
-  _Wi++;
-  i++;  
-  }
-
-  
-  //carregando vetemp_key
-  for(linha=0; linha<4 ; linha++){
-    for(coluna=0;coluna<4; coluna++){
-      chave[linha][coluna]= subchaves[linha][_Wi-4+coluna];
+  for(int i=0; i<4; i++){
+    for(int j=0; j<4; j++){
+      matrizcopia[i][j] = chave[i][j];
     }
   }
+
+  for(int i=0; i<4; i++){    
+      copia[i] = chave[i][3];      
+  }
+
+
+  byte aux = copia[0];
+  copia[0] = copia[1];
+  copia[1] = copia[2];
+  copia[2] = copia[3];
+  copia[3] = aux;
+
+  for(int i=0; i<4; i++){
+    copia[i] = sub(copia[i]);
+  }
+  
+  /*if(round == 1){
+     for(int i=0; i<4; i++){
+      if(i==0){
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x01;  
+      }else{
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x00;
+      }
+  }  
+  }*/
+  
+   if(round < 9){
+    for(int i=0; i<4; i++){
+      if(i==0){
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ Rcon[round -1];  
+      }else{
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x00;
+      }
+  }  
+   }else if(round == 9){
+    for(int i=0; i<4; i++){
+      if(i==0){
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x1b;  
+      }else{
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x00;
+      }
+    }
+   }else {
+    for(int i=0; i<4; i++){
+      if(i==0){
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x36;  
+      }else{
+        matrizcopia[i][0] = chave[i][0] ^ copia[i] ^ 0x00;
+      }
+   }}
+
+  for(int i=1; i<4; i++){
+    matrizcopia[0][i] = chave[0][i] ^ matrizcopia[0][i-1];
+    matrizcopia[1][i] = chave[1][i] ^ matrizcopia[1][i-1];
+    matrizcopia[2][i] = chave[2][i] ^ matrizcopia[2][i-1];
+    matrizcopia[3][i] = chave[3][i] ^ matrizcopia[3][i-1];
+  }
+for(int i=0; i<4; i++){
+    for(int j=0; j<4; j++){
+      chave[i][j] = matrizcopia[i][j];
+    }
+  }
+   
 }
