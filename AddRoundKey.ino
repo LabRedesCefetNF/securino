ff0528b9cff07aef07f37aea11fff3154c19ebc8
Nilson Mori <nilsonmori@yahoo.com>
2019-06-26 23:14:31 -0300

    Biblioteca Versao 1.0

--- AddRoundKey.ino
+++
@@ -1,9 +0,0 @@
-byte* AddRoundKey(byte state[][4], byte roundkey[][4]){
-  for (int i = 0; i < 4; i++)
-  {
-    for(int j=0; j < 4; j++)
-    {
-      state[i][j] ^= roundkey[i][j];     
-    }
-  }
-}
---
+++ Securino/Securino.h
@@ -0,0 +1,54 @@
+/*
+  Javino.h - Library communication for Arduino and Jason.
+Version Stable 1.1
+  Created by Lazarin, NM and Pantoja, CE - January 29, 2015.
+    nilson.lazarin@cefet-rj.br
+	carlos.pantoja@cefet-rj.br
+
+  Updated in 2015-06-21
+  Released into the public domain.
+
+*/
+
+#ifndef Securino_h
+#define Securino_h
+#define quatro 4
+#define dezesseis 16
+#include "Arduino.h"
+
+class Securino{
+  public:
+    Securino();
+    byte *encript(String operacao, byte in[dezesseis], byte k[dezesseis] );
+    byte *decript(String operacao, byte in[dezesseis], byte k[dezesseis] );
+  private:
+    byte plaintext[quatro][quatro];
+    byte key[quatro][quatro];                 
+    byte keyCopia[quatro][quatro];
+    void setKey(byte x[dezesseis]);
+    void setData(byte x[dezesseis]);
+    byte *getData();
+    void encAES128ecb(byte plainText[][quatro], byte chave[][quatro]);
+    void decAES128ecb(byte plainText[][quatro], byte chave[][quatro]);
+    byte* AddRoundKey(byte state[][quatro], byte roundkey[][quatro]);
+    byte tableS(byte linha, byte coluna);
+    byte tableInvS(byte linha, byte coluna);
+    byte subX(byte valor);
+    byte invSub(byte valor);
+    void subBytes(byte state[][quatro]);
+    void InvSubBytes(byte state[][quatro]);
+    void SubBytesKey(byte chave[]);
+    void ShiftRows(byte state[][quatro]);
+    void invShiftRows(byte state [][quatro]);
+    byte tableE(byte linha, byte coluna);
+    byte tableL(byte linha, byte coluna);
+    byte L(byte valor);
+    byte E(byte valor);
+    byte calculaLE(byte valor1, byte valor2);
+    void MixColumns(byte matrizEntrada[][quatro]);
+    void MixColumnsInversa(byte matrizResultante[][quatro]);
+    void KeySchedule(byte chave[][quatro], int round);
+    void InvKeySchedule(byte chave[][quatro], int rodada);
+};
+
+#endif
